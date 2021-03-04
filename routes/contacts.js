673826bae2a0eca1cbdf9de94a563c1a97af2d4f
sravanthi366 <sravanthimca95@gmail.com>
2021-03-04 18:34:55 +0530

    prepare for production

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,21 +1,57 @@
 const express = require('express')
 const router = express.Router();
+const auth = require('../middleware/auth')
+const { check, validationResult } = require('express-validator');
+
+const User = require('../model/User')
+const Contact = require('../model/Contact')
 
 //@ route      GET  api/contacts
 //@desc        GET all user contacts
 //@ access     Private
-router.get('/', (req, res) => {
+router.get('/', auth, async(req, res) => {
     
-    res.send('GET all user contacts')
+    try {
+        const contacts = await Contact.find({user:req.user.id}).sort({date: -1})
+    res.json(contacts)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Internal Server Error')
+    }
 });
 
 //@ route      POST  api/contacts
 //@desc        ADD new Contact
 //@ access     Private
 
-router.post('/', (req, res) => {
+router.post('/', [auth,
+check('name',"name is required").not().isEmpty()
+
+], async (req, res) => {
     
-    res.send('ADD contact')
+    const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors:errors.array()})
+        }
+
+        const { name, email, type, phone } = req.body
+        try {
+
+            const newContact = new Contact({
+                name,
+                email,
+                phone,
+                type,
+                user:req.user.id
+
+            })
+            
+            const contact = await newContact.save()
+            res.json(contact)
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send('Internal Server Error')
+        }
 });
 
 
@@ -23,18 +59,54 @@ router.post('/', (req, res) => {
 //@desc        Update Contact
 //@ access     Private
 
-router.put('/:id', (req, res) => {
+router.put('/:id', auth,async(req, res) => {
     
-    res.send('Update contact')
+    const { name, phone, email, type } = req.body;
+    // Build contact object
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
+    try {
+        let contact = await Contact.findById(req.params.id)
+        if (!contact) return res.status(404).json({ msg: 'Contact not found' })
+        //Make sure user owns contact
+        if (contact.user.toString() !== req.user.id) {
+            res.status(401).json({msg:"Not Authorized"})
+        }
+        contact = await Contact.findByIdAndUpdate(req.params.id,
+            { $set: contactFields },
+            { new: true });
+        res.json(contact);
+    } catch (error) {
+          console.error(error.message)
+            res.status(500).send('Internal Server Error')
+    }
+
+    
 });
 
 //@ route      DELETE  api/contacts/:id
 //@desc        Delete Contact
 //@ access     Private
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', auth,async(req, res) => {
     
-    res.send('Delete contact')
+    try {
+        let contact = await Contact.findById(req.params.id)
+        if (!contact) return res.status(404).json({ msg: 'Contact not found' })
+        //Make sure user owns contact
+        if (contact.user.toString() !== req.user.id) {
+            res.status(401).json({msg:"Not Authorized"})
+        }
+       await Contact.findByIdAndRemove(req.params.id)
+        res.json({msg:"Contact removed"});
+    } catch (error) {
+          console.error(error.message)
+            res.status(500).send('Internal Server Error')
+    }
+
 });
 
 
--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require('express');
-
+const connectDB = require('./config/db')
+const path = require('path')
 const app = express();
 
+// connect Database
 
-app.get('/', (req, res) => {
-    res.json({msg:"Hello contact-keeper API ..."})
-})
+connectDB();
+
+//init middleware
+
+app.use(express.json({extended:false}))
+
+
+
 
 // Define Routes
 
 app.use('/api/users',require('./routes/users'))
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/contacts',require('./routes/contacts'))
+//serve static assets in production
+
+if(process.env.NODE_ENV==='production'){
+    //set static folder
+
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')))
+
+
+}
 
 const PORT = process.env.PORT || 5000;
 
